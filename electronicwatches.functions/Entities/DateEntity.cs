c118fc4f00998346edc4e3ad93116b42acced1bd
andres <maicoltrujillo382@gmail.com>
2021-08-28 14:16:26 -0500

    Date Entity

---
+++ electronicwatches.functions/Entities/DateEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace electronicwatches.functions.Entities
+{
+     public class DateEntity: TableEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime DateWorked { get; set; }
+
+        public int Hours { get; set; }
+    }
+}
